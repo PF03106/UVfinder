4f5547adb0183363686f8815bf63e73d6ba4c326
Seyeon Kim <gofl4520@gmail.com>
2026-02-20 15:13:20 -0500

    new feat, chromomap plotting Rscript

--- workflow/scripts/plot_chromomap.R
+++ workflow/scripts/plot_chromomap.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+# Users should create sex_linked_genes.txt file with one gene name per line (header: genes)(option -g or --genes) and specify mode (option -m or --mode) and target (option -t or --target) if needed.
 
 library(optparse)
 library(tidyverse)
@@ -10,10 +11,10 @@ library(data.table)
 # -------------------------------
 # 1. Set path
 # -------------------------------
-genome_base <- "resources/genomes"
-blast_base <- "68_results/03_locus_search"
+genome_base <- "results/00_renamed"
+blast_base <- "results/03_locus_search"
 meta_file <- "config/samples.tsv"
-out_dir <- "68_results/test_chromomap"
+out_dir <- "results/test_chromomap"
 dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
 
 # -------------------------------
@@ -27,7 +28,7 @@ option_list = list(
   make_option(c("-t", "--target"), type="character", default=NULL, 
               help=" If mode is set to 'order' or 'sample_id', target name (e.g. Dicriales or S001, S002, ... sep = , ", metavar="character"),
   make_option(c("-b", "--blast_type"), type="character", default="best_hits", 
-              help=" Choose between best_hits and all_hits", metavar="character")
+              help=" Choose between best_hit and all_hits", metavar="character")
 )
 
 opt_parser = OptionParser(option_list=option_list)
@@ -70,74 +71,79 @@ message(sprintf("Creating chromoMap for %d samples...", length(target_samples)))
 # 4. Loop through target samples to create chromoMap visualizations
 # -------------------------------
 std_cols <- c("qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", 
-              "qstart", "qend", "sstart", "send", "evalue", "bitscore")
+              "qstart", "qend", "sstart", "send", "evalue", "bitscore", "rank", "sex_tag")
 
 for (sample in target_samples) {
   message("Processing: ", sample)
 
-  ref_fa <- meta_df$genome_filename[meta_df$sample_id == sample]
-  blast_dir <- file.path(blast_base, sample, opt$blast_type, )
-  
-  if (is.na(ref_fa) || !file.exists(ref_fa)) {
-    message("❌ [ERROR] Reference genome file not found for sample: ", sample)
-    message("   -> Looked for path: ", ref_fa)
-    message("   -> Skipping this sample and moving to the next...")
+  ref_fa <- file.path(genome_base, paste0(sample, "_renamed.fasta"))
+  if (length(ref_fa) == 0 || is.na(ref_fa[1]) || !file.exists(ref_fa[1])) {
+    message("❌ [ERROR] Reference genome file not found for sample: ", ref_fa, " -> skipping sample.")
     next
   }
-  
-  # read reference genome and create chromosome length map
+  # create chrlen_map and chrom_file.
   ref <- readDNAStringSet(ref_fa)
   chrlen_map <- setNames(width(ref), names(ref))
   
-  # create chromoMap input file for chromosome information
   chrom_file <- file.path(out_dir, paste0(sample, "_chrom.txt"))
   data.frame(
     chrom = names(chrlen_map),
     start = 1,
     end = as.integer(chrlen_map)
   ) %>% write_tsv(chrom_file, col_names = FALSE)
-  
-  # Read BLAST files
-  blast_files <- list.files(blast_dir, pattern = "\\.txt$", full.names = TRUE)
-  if (length(blast_files) == 0) {
-    warning("BLAST result files not found: ", blast_dir, " -> skipping sample.")
+
+  blast_dir <- file.path(blast_base, sample)
+
+  target_file_name <- ifelse(opt$blast_type == "all_hits", 
+                           "A_All_hits.tsv", 
+                           "B_Best_hits.tsv")
+  blast_file <- file.path(blast_dir, target_file_name)
+
+  dt <- fread(blast_file, header=TRUE, col.names = std_cols, sep="\t", fill=TRUE)
+  if (nrow(dt) == 0) {
+    warning("BLAST result is empty for sample: ", sample, " -> skipping sample.")
     next
+  } else {
+    message("✔ BLAST results loaded: ", blast_file)
+    all_hits <- dt %>% mutate(
+      start = pmin(sstart, send),
+      end   = pmax(sstart, send),
+      probe = qseqid
+    )
   }
-  
-  blast_list <- lapply(blast_files, function(f){
-    dt <- fread(f, header=FALSE, col.names = std_cols, sep="\t", fill=TRUE)    
-    if (nrow(dt) > 0) {
-      dt %>% mutate(
-        start = pmin(sstart, send),
-        end   = pmax(sstart, send),
-        probe = qseqid
-      )
-    }
-  })
-  
-  all_hits <- rbindlist(blast_list)
-  
-  if (nrow(all_hits) == 0) {
-    warning("BLAST hits are 0 for sample: ", sample, " -> skipping sample.")
-    next
-  }
-  
   # -------------------------------
   # 5. Prepare annotation file for chromoMap (probe locations)
   # -------------------------------
-  annot_file <- file.path(out_dir, paste0(sample, "_annotation.txt"))
-    all_hits %>% 
+  annot_df <- all_hits %>% 
     arrange(sseqid, start, end) %>%
     transmute(
       ElementName    = probe,
       ChromosomeName = sseqid,
       Start          = as.integer(start),
       End            = as.integer(end),
-      Category       = ifelse(probe %in% sex_lniked_genes, "1_Target", "2_Other")
+      Category       = ifelse(sub("\\|.*", "", probe) %in% sex_lniked_genes, "1_Target", "2_Other")
     ) %>% 
-    filter(ChromosomeName %in% names(chrlen_map)) %>%
-    write_tsv(annot_file, col_names = FALSE)
+    filter(ChromosomeName %in% names(chrlen_map))
+
+  if(nrow(annot_df) == 0) {
+    warning("No annotations left after filtering for sample: ", sample, " -> skipping.")
+    next
+  }
+
+  annot_file <- file.path(out_dir, paste0(sample, "_annotation.txt"))
+  write_tsv(annot_df, annot_file, col_names = FALSE)
   
+  present_categories <- sort(unique(annot_df$Category))
+  ordered_categories <- unique(annot_df$Category)
+  
+  sample_colors <- c()
+  for (cat in ordered_categories) {
+    if (cat == "1_Target") {
+      sample_colors <- c(sample_colors, "#FF0066")
+    } else if (cat == "2_Other") {
+      sample_colors <- c(sample_colors, "#0099CC")
+    }
+  }
   # -------------------------------
   # 6. Visualize with chromoMap and save svg
   # -------------------------------
@@ -150,13 +156,17 @@ for (sample in target_samples) {
     data.files = annot_file,
     data_based_color_map = TRUE,
     data_type = "categorical",
-    data_colors = list(c("red", "blue")),
+    data_colors = list(sample_colors),
     segment_ann = TRUE,
     chr_color   = "#bdbdbd",
     ploidy      = 1,
     canvas_width  = 1400,
     canvas_height = 700,
-    title = title
+    title = title, 
+    export.options = list(
+      format = "svg",
+      filename = file.path(out_dir, paste0(title, ".svg"))
+    )
   )
   
   saveWidget(w, file = html_file, selfcontained = TRUE)
